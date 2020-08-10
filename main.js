cc0dd8352b8083a625200fb622d7a1c6623a2416
Farzad Ghaznavi <ghfarzad@gmail.com>
2020-08-10 16:16:02 -0400

    Set cursor on model's Bbox

--- main.js
+++ main.js
@@ -1,16 +1,11 @@
-function changeColor(el, color) {
-  el.setAttribute('color', color);
-}
-
 function hidePopup(el) {
-    changeColor(el, 'grey');
     var modal = document.getElementById("myModal");
     modal.style.display = "none";
 }
 
-function showPopup(el) {
+function showPopup(elementName) {
     var content = {
-        'rethink'   : {
+        'rethink' : {
             'definition' : 'How do we decouple economic growth from finite resource consumption? This requires rethinking and redefining our behaviours as consumers and citizens, as well as what our cities could look like in the future.',
             'funFact'    : 'Did you know?',
             'hyperlinks' : [
@@ -38,7 +33,7 @@ function showPopup(el) {
                 }
             ]
         },
-        'reuse'     : {
+        'reuse' : {
             'definition' : 'Prolonging an asset’s life at its optimum value by maximizing utilization and finding inventive new uses through new ways of collaborating and re-examining supply chains.',
             'funFact'    : 'Did you know? 100% of the PVC pipes used for this installation are construction scrap that would have otherwise gone to landfill.',
             'hyperlinks' : [
@@ -60,12 +55,12 @@ function showPopup(el) {
         }
     };
 
-    document.getElementById('contentDef').innerHTML = content[el.getAttributeNames()[0]]['definition'];
-    document.getElementById('contentFact').innerHTML = content[el.getAttributeNames()[0]]['funFact'];
+    document.getElementById('contentDef').innerHTML = content[elementName]['definition'];
+    document.getElementById('contentFact').innerHTML = content[elementName]['funFact'];
 
     document.getElementById('contentLinks').innerHTML = '';
 
-    content[el.getAttributeNames()[0]]['hyperlinks'].forEach(function (item, index) {
+    content[elementName]['hyperlinks'].forEach(function (item, index) {
         var node1 = document.createElement("LI");
         var node2 = document.createElement("A");
         node2.href = item['link'];
@@ -119,84 +114,118 @@ window.addEventListener("load", cameraStart, false);
 
 function initializeElement(el)
 {
-    el.setAttribute('scale',    {'x' :  0.1, 'y' :  0.1, 'z' :  0.1 });
+    el.setAttribute('scale',    { 'x' : 0.1, 'y' :  0.1, 'z' :  0.1 });
     el.setAttribute('position', { 'x' : 0,   'y' :  0,   'z' : -3   });
-    el.setAttribute('rotation', { 'x' : 0,   'y' : -20,  'z' :  0   });
 }
 
-// Component to change to a sequential color on cursor suspension.
-AFRAME.registerComponent('cursor-listener', {
-  init: function () {
-    var lastIndex = -1;
-    var COLORS = ['red', 'green', 'blue'];
-    this.el.addEventListener('click', function (evt) {
-      lastIndex = (lastIndex + 1) % COLORS.length;
-      this.setAttribute('material', 'color', COLORS[lastIndex]);
-      console.log('I was clicked at: ', evt.detail.intersection.point);
+function initializeMdoelBoundingBox(el)
+{
+    //var mesh = el.getObject3D('mesh');
+    //var bbox = new THREE.Box3().setFromObject(mesh);
+    //var centerX = (bbox.max.x + bbox.min.x) / 2;
+    //var centerY = (bbox.max.y + bbox.min.y) / 2;
+    //var centerZ = (bbox.max.z + bbox.min.z) / 2;
+
+    //var centroid = { 'x' : centerX, 'y': centerY, 'z': centerZ }
+    //var depth    = Math.abs(bbox.max.z - bbox.min.z);
+    //var height   = Math.abs(bbox.max.y - bbox.min.y);
+    //var width    = Math.abs(bbox.max.x - bbox.min.x);
+    //var rotation = el.getAttribute('rotation');
+
+    // TODO: Couldn't reliably get sbbox spec at runtime
+    // as 'model-loaded' event seem to get fired prematurley.
+    var specs = {
+        'regenerate' : {
+            'centroid': {x: 19.4721050262451, y: 1.694560706615445, z: -0.125},
+            'depth': 1.75,
+            'height': 5.87168753147125,
+            'rotation': {x: 0, y: 0, z: 0},
+            'width': 38.9442100524902
+        },
+        'rethink' : {
+            'centroid': {x: 13.54986095428465, y: 2.3901673816144484, z: -0.125},
+            'depth': 1.75,
+            'height': 4.933751769363884,
+            'rotation': {x: 0, y: 0, z: 0},
+            'width': 27.0997219085693
+        },
+        'reduce' : {
+            'centroid': {x: 12.9780330657959, y: 2.3430962227284886, z: -0.125},
+            'depth': 1.75,
+            'height': 4.8326359465718225,
+            'rotation': {x: 0, y: 0, z: 0},
+            'width': 25.9560661315918
+        },
+        'reuse' : {
+            'centroid': {x: 10.9138765335083, y: 1.7625523470342137, z: -0.125},
+            'depth': 1.75,
+            'height': 3.6715481951832727,
+            'rotation': {x: 0, y: 0, z: 0},
+            'width': 21.8277530670166
+        }
+    }
+
+    var spec = specs[el.getAttributeNames()[0]];
+    var box = document.createElement('a-box');
+    box.setAttribute('depth',    spec.depth);
+    box.setAttribute('height',   spec.height);
+    box.setAttribute('width',    spec.width);
+    box.setAttribute('position', spec.centroid);
+    box.setAttribute('rotation', spec.otation);
+    box.setAttribute('color',    'red');
+    box.setAttribute('visible',  false);
+
+    box.addEventListener('click', function () {
+        showPopup(el.getAttributeNames()[0]);
     });
-  }
-});
+    box.addEventListener('mouseleave', function () {
+        hidePopup(el.getAttributeNames()[0]);
+    });
+
+    el.appendChild(box);
+}
 
 AFRAME.registerComponent('regenerate', {
 init: function () {
-  var data = this.data;
   var el = this.el;
 
-  el.addEventListener('click', function () {
-      showPopup(el);
-  });
-  el.addEventListener('mouseleave', function () {
-      hidePopup(el);
-  });
-
   initializeElement(el);
+  el.addEventListener('model-loaded', (e) => {
+    initializeMdoelBoundingBox(el);
+  });
 }
 });
 
 AFRAME.registerComponent('reduce', {
 init: function () {
-  var data = this.data;
   var el = this.el;
 
-  el.addEventListener('click', function () {
-      showPopup(el);
-  });
-  el.addEventListener('mouseleave', function () {
-      hidePopup(el);
-  });
-
   initializeElement(el);
+  el.addEventListener('model-loaded', (e) => {
+    initializeMdoelBoundingBox(el);
+  });
 }
 });
 
 AFRAME.registerComponent('rethink', {
 init: function () {
-  var data = this.data;
   var el = this.el;
 
-  el.addEventListener('click', function () {
-      showPopup(el);
-  });
-  el.addEventListener('mouseleave', function () {
-      hidePopup(el);
-  });
-
   initializeElement(el);
+  el.addEventListener('model-loaded', (e) => {
+    initializeMdoelBoundingBox(el);
+  });
 }
 });
 
 AFRAME.registerComponent('reuse', {
 init: function () {
-  var data = this.data;
   var el = this.el;
 
-  el.addEventListener('click', function () {
-      showPopup(el);
-  });
-  el.addEventListener('mouseleave', function () {
-      hidePopup(el);
-  });
-
   initializeElement(el);
+  el.addEventListener('model-loaded', (e) => {
+    initializeMdoelBoundingBox(el);
+  });
 }
 });
+
